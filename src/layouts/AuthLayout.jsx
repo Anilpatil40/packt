225310d45e218ad167328c57b162ccb34b10da0e
Anil Patil <panil8993@gmail.com>
2023-11-06 01:04:31 +0530

    navigation from auth pages

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,9 +1,17 @@
 import { Stack, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 export const AuthLayout = ({ children }) => {
+    const navigate = useNavigate();
     return (
         <Stack className="vh-100 p-4 align-items-center">
-            <Typography variant="h3" fontWeight={"bold"} color={"primary"}>
+            <Typography
+                variant="h3"
+                fontWeight={"bold"}
+                color={"primary"}
+                onClick={() => navigate("/")}
+                sx={{ cursor: "pointer" }}
+            >
                 {"<packt>"}
             </Typography>
             {children}
